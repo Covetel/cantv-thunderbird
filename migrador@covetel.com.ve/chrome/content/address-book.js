4c2f7bc490f4aabfb7e93a35069315381de8ebf1
Antonio Jose Lopez <antoniojlopez@gmail.com>
2012-12-07 00:55:38 -0430

    refs #81 Migración de contactos funcional

--- migrador@covetel.com.ve/chrome/content/address-book.js
+++ migrador@covetel.com.ve/chrome/content/address-book.js
@@ -1,58 +1,27 @@
-let addressBookSynchronizationObserver = {
-    syncCount: 0,
-    handleNotification: function(notification, data) {
-
-	Application.console.log("Notificacion: " + notification + " " + data);
-
-        if (notification == "groupdav.synchronization.stop") {
-		this.syncCount++;
-        }
-
-	// Only 2 addressbooks are synchronized
-	if (this.syncCount == 2) {
-
-			Application.console.log("Entro aqui!");
-			ABookMigrator.unloadObservers();
-			ABookMigrator.moveAddressBook();
-			/*startFolderSync();
-			ABookMigrator.removeAddressBooks();*/
-	}
-    }
-};
-
 var ABookMigrator = {
 	abManager : null,
+	nManager: null,
 	inputAb : null,
 	outputAb : null,
 	init: function() {
 		this.abManager = Components.classes["@mozilla.org/abmanager;1"]
-		                      .getService(Components.interfaces.nsIAbManager);
+		                 .getService(Components.interfaces.nsIAbManager);
+
+		this.nManager = Components.classes["@inverse.ca/notification-manager;1"]
+				     .getService(Components.interfaces.inverseIJSNotificationManager)
+				     .wrappedJSObject;
 	},
 	start: function(resume) {
 		this.removeAddressBooks();
 		this.createAddressBooks();
 		this.setCredentials();
-	    	this.loadObservers();
+		this.nManager.registerObserver("groupdav.synchronization.stop", { 
+			handleNotification: function(notification, data) {
+				ABookMigrator.nManager.unregisterObserver("groupdav.synchronization.stop", this);
+				ABookMigrator.copyAddressBook();
+				SynchronizeGroupdavAddressbook(ABookMigrator.outputAb.URI, null, null);
+			}});
 		startFolderSync();
-	},
-	
-	loadObservers: function() {
-
-		let nmgr = Components.classes["@inverse.ca/notification-manager;1"]
-				     .getService(Components.interfaces.inverseIJSNotificationManager)
-				     .wrappedJSObject;
-
-  		nmgr.registerObserver("groupdav.synchronization.start", addressBookSynchronizationObserver);
-  		nmgr.registerObserver("groupdav.synchronization.stop", addressBookSynchronizationObserver);
- 	},
-	unloadObservers: function() {
-
-		let nmgr = Components.classes["@inverse.ca/notification-manager;1"]
-				     .getService(Components.interfaces.inverseIJSNotificationManager)
-				     .wrappedJSObject;
-
-  		nmgr.unregisterObserver("groupdav.synchronization.start", addressBookSynchronizationObserver);
-  		nmgr.unregisterObserver("groupdav.synchronization.stop", addressBookSynchronizationObserver);
 	},
 	removeAddressBooks: function() {
 		
@@ -63,6 +32,7 @@ var ABookMigrator = {
 			let directory = directories.getNext().QueryInterface(Components.interfaces.nsIAbDirectory);
 
 			if (directory instanceof Components.interfaces.nsIAbDirectory) {
+
 				// Only removes remote addressbooks
 				try {			
 					SCDeleteDAVDirectory(directory.URI);
@@ -85,18 +55,7 @@ var ABookMigrator = {
 		outputURL = outputURL.replace("#DOMAIN", AccountManager.getDomain());		
 
 		this.inputAb = SCCreateGroupDAVDirectory("input", inputURL);
-		this.outputAb = SCCreateGroupDAVDirectory("output", outputURL);
-	},
-	createFinalAddressBooks: function() {
-
-		var prefs = Components.classes["@mozilla.org/preferences-service;1"]
-					.getService(Components.interfaces.nsIPrefService)
-					.getBranch("extensions.migration.");	
-
-		var url = prefs.getCharPref("addressbook.output.url").replace("#USER", AccountManager.getUsername());
-		url = url.replace("#DOMAIN", AccountManager.getDomain());		
-
-		SCCreateGroupDAVDirectory(prefs.getCharPref("extensions.migration.addressbook.name"), url);
+		this.outputAb = SCCreateGroupDAVDirectory(prefs.getCharPref("addressbook.name"), outputURL);
 	},
 	//TODO: Delete credentials when migration is finished
 	setCredentials: function() {
@@ -125,79 +84,62 @@ var ABookMigrator = {
 	
 		// output service
 		var authLoginInfoServer2 = new nsLoginInfo(
-		    'http://sogo-demo.inverse.ca',
+		    'https://190.142.238.79:8081',
 		    null,
 		    'SOGo',
-		    /*DEBUG AccountManager.getUsername()*/"sogo1",
-		    /*DEBUG: password */"sogo1",
+		    /*DEBUG AccountManager.getUsername()*/"emujic",
+		    /*DEBUG: password */"123456",
 		    "",
 		    ""
 		);
 
 		if (!passwordManager.getLoginSavingEnabled("http://localhost:1080"))
 			passwordManager.addLogin(authLoginInfoServer1);
-		if (!passwordManager.getLoginSavingEnabled("http://sogo-demo.inverse.ca"))
+		if (!passwordManager.getLoginSavingEnabled("https://190.142.238.79:8081"))
 		passwordManager.addLogin(authLoginInfoServer2); 
 
 
 	},
-   	moveAddressBook: function() {
+   	copyAddressBook: function() {
 
-		Application.console.log("Depurando...");
+		let childCards = this.inputAb.childCards;
 
-		let abManager = Components.classes["@mozilla.org/abmanager;1"]
-		                          .getService(Components.interfaces.nsIAbManager);
+		while (childCards.hasMoreElements()) {
 
-		if (this.inputAb.URI != this.outputAb.URI) {
-		    /* ugly hack: we empty the addressbook after its cards were
-		     transfered, so that we can be sure the ab no longer "exists" */
-		    let cardsArray = Components.classes["@mozilla.org/array;1"]
-		                               .createInstance(Components.interfaces.nsIMutableArray);
+			let card = childCards.getNext().QueryInterface(Components.interfaces.nsIAbCard);
 
-		    let childCards = this.inputAb.childCards;
-		    let countCards = 0;
-		    let countLists = 0;
-		    while (childCards.hasMoreElements()) {
-		        let card = childCards.getNext().QueryInterface(Components.interfaces.nsIAbCard);
 		        if (card.isMailList) {
-		            let oldListDir = abManager.getDirectory(card.mailListURI);
-		            let listDir = Components.classes["@mozilla.org/addressbook/directoryproperty;1"]
+
+		        	let oldListDir = this.abManager.getDirectory(card.mailListURI);
+		        	let listDir = Components.classes["@mozilla.org/addressbook/directoryproperty;1"]
 		                                    .createInstance(Components.interfaces.nsIAbDirectory);
-		            listDir.isMailList = true;
+				listDir.isMailList = true;
+				listDir.dirName = oldListDir.dirName;
+				listDir.listNickName = oldListDir.listNickName;
+				listDir.description = oldListDir.description;
 
-		            listDir.dirName = oldListDir.dirName;
-		            listDir.listNickName = oldListDir.listNickName;
-		            listDir.description = oldListDir.description;
-
-		            for (let i = 0; i < oldListDir.addressLists.length; i++) {
-		                let subcard = oldListDir.addressLists.queryElementAt(i, Components.interfaces.nsIAbCard);
-		                let cloneCard = Components.classes["@mozilla.org/addressbook/moz-abmdbcard;1"]
+				for (let i = 0; i < oldListDir.addressLists.length; i++) {
+					let subcard = oldListDir.addressLists.queryElementAt(i, Components.interfaces.nsIAbCard);
+					let cloneCard = Components.classes["@mozilla.org/addressbook/moz-abmdbcard;1"]
 		                                          .createInstance(Components.interfaces.nsIAbCard);
-		                cloneCard.copy(subcard);
-		                listDir.addressLists.appendElement(cloneCard, false);
-		            }
-		            this.outputAb.addMailList(listDir);
-		            countLists++;
+					cloneCard.copy(subcard);
+					cloneCard.setProperty("groupDavVersion", "-1");
+					listDir.addressLists.appendElement(cloneCard, false);
+				}
+
+				this.outputAb.addMailList(listDir);
 		        }
 		        else {
-		            let cloneCard = Components.classes["@mozilla.org/addressbook/moz-abmdbcard;1"]
-		                                      .createInstance(Components.interfaces.nsIAbCard);
-		            cloneCard.copy(card);
-		            this.outputAb.addCard(cloneCard);
-		            countCards++;
+
+				if (card.getProperty("groupDavKey", "NONE") != "NONE") {
+					
+					let cloneCard = Components.classes["@mozilla.org/addressbook/moz-abmdbcard;1"]
+				                              .createInstance(Components.interfaces.nsIAbCard);
+					cloneCard.copy(card);
+					cloneCard.setProperty("groupDavVersion", "-1");
+					this.outputAb.addCard(cloneCard);
+				}
 		        }
-		        cardsArray.appendElement(card, false);
-		    }
-		    this.inputAb.deleteCards(cardsArray);
-		    this.inputAb.QueryInterface(Components.interfaces.nsIAbMDBDirectory).database.close(true);
-		    if (countCards || countLists) {
-		        dump("moved " + countCards + " cards and "
-		             + countLists + " lists from " + this.inputAb.URI
-		             + " to " + this.outputAb.URI + "\n");
-		    }
-		}
-		else {
-		    dump("_moveAddressBook: source and destination AB are the same\n");
 		}
 	}
 };
--- migrador@covetel.com.ve/defaults/preferences/default.js
+++ migrador@covetel.com.ve/defaults/preferences/default.js
@@ -1,7 +1,7 @@
 pref("extensions.migration.version","1.0");
 pref("extensions.migration.domain", "cantv.com.ve");
 pref("extensions.migration.addressbook.input.url",  "http://localhost:1080/users/#USER@#DOMAIN/contacts/");
-pref("extensions.migration.addressbook.output.url", "http://sogo-demo.inverse.ca/SOGo/dav/sogo1/Contacts/personal/");
+pref("extensions.migration.addressbook.output.url", "https://190.142.238.79:8081/SOGo/dav/emujic/Contacts/personal");
 pref("extensions.migration.addressbook.name",  "Libreta remota");
 
 
